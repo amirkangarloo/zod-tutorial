4fffe6353e6ccf70c9bcf6d98d613061ee2d988b
Amir Kangarloo <amirkangarloo1051@gmail.com>
2025-02-02 13:09:39 +0330

    Solved challenges 04 and 05

--- src/04-infer.problem.ts
+++ src/04-infer.problem.ts
@@ -10,7 +10,9 @@ const StarWarsPeopleResults = z.object({
   results: z.array(StarWarsPerson),
 });
 
-const logStarWarsPeopleResults = (data: unknown) => {
+type Data = z.infer<typeof StarWarsPeopleResults>
+
+const logStarWarsPeopleResults = (data: Data) => {
   //                                    ^ 🕵️‍♂️
   data.results.map((person) => {
     console.log(person.name);
