4468d91f25a52618bf2b9c39c45208ba52a7ef6a
Amir Kangarloo <amirkangarloo1051@gmail.com>
2025-02-02 11:35:46 +0330

    Solved challenge 02

--- src/02-object.problem.ts
+++ src/02-object.problem.ts
@@ -3,7 +3,11 @@
 import { expect, it } from "vitest";
 import { z } from "zod";
 
-const PersonResult = z.unknown();
+const PersonResult = z.object({
+  name: z.string(),
+  height: z.string(),
+  // TODO: Add another keys
+});
 //                   ^ 🕵️‍♂️
 
 export const fetchStarWarsPersonName = async (id: string) => {
